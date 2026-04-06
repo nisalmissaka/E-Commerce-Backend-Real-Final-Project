9716ddc17d9b6801766daa228b54e80340d12523
nisalmissaka <wishwakanisal@gmail.com>
2026-04-06 09:36:08 +0530

    Added id as an auto-incrementing primary key.

---
+++ src/main/java/edu/example/model/entity/Role.java
@@ -0,0 +1,16 @@
+package edu.example.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
